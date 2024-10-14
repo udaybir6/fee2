189815ebf70c3d82d44e125ec0e5bf14beb20674
udaybir6 <143985062+udaybir6@users.noreply.github.com>
2024-10-14 11:24:15 +0530

    Add files via upload

---
+++ Class Work/javascript/DomEvents.js
@@ -0,0 +1,30 @@
+// var btn;
+// var btns = document.QuerySelectorall("button");
+// for (btn of buttons){
+//     btn.addEventListener("click",myfunc);
+//     btn.addEventListener("click",hello);
+// }
+// function myfunc(){
+//     console.log("button was pressed")
+// }
+// function myfunc(){
+//     console.log("hello")
+// }
+// console.dir(btn);
+
+// var btn = document.querySelector('p');
+// btn.onmouseover = paragraph;
+// function paragraph(){
+//     alert("paragraph")
+// }
+var form  =document.querySelector("form");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        console.log("form submitted");
+    });var form  =document.querySelector("form");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        console.log("form submitted");
+    });
