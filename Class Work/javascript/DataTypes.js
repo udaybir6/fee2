fb20162aeea1e60c17d1e54ad7b10a0d70f10433
udaybir6 <143985062+udaybir6@users.noreply.github.com>
2024-10-14 11:23:32 +0530

    Add files via upload

---
+++ Class Work/javascript/DataTypes.js
@@ -0,0 +1,39 @@
+// 1. Number: +,-,decimal,integer
+
+let a = 10;
+let b = -5
+let c = 1.2
+console.log(a)
+console.log(typeof(a))
+console.log(b)
+console.log(typeof(b))
+console.log(c)
+console.log(typeof(c))
+
+
+// 2.string:
+let name = "tarun"
+console.log(name)
+console.log(typeof(name))
+
+// 3.boolean:
+let i = true;
+console.log(typeof(i))
+
+// 4. undefined:
+let y;
+console.log(typeof(y))
+y = 5;
+console.log(y)
+
+// 5. null
+let j = null;
+console.log(typeof(j))
+
+// 6. sort
+let arr = [1,2,3,4,5,6,7,8,9,10]
+console.log(arr.sort())
+
+// 7. reverse
+let arr1 = [1,2,3,4,5,6,7,8,9,10]
+console.log(arr1.reverse())
