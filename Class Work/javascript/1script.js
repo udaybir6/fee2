9fcf110c18faa257cf734fef6c49eeeabfd64b23
udaybir6 <143985062+udaybir6@users.noreply.github.com>
2024-10-14 11:17:51 +0530

    Add files via upload

---
+++ Class Work/javascript/1script.js
@@ -0,0 +1,12 @@
+var newh1 = document.querySelector("h1");
+
+// 1. add
+console.dir(newh1.classList);
+newh1.classList.add("green");
+
+// 2. remove
+console.dir(newh1.classList[1]);
+newh1.classList.remove("green");
+
+var b = new1.classList.ccontains("green");
+
